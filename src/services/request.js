97783cbf246c817f5480ee8a5962232ccbfa1b83
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2022-07-30 00:23:01 +0300

    add spinner for loading , add gif for error

---
+++ src/services/request.js
@@ -0,0 +1,32 @@
+class MarvelInfo {
+    _apiKey = "9e0a30c0c5b259f9360bba6b1f9e4410"
+    getInfo = async (url) => {
+        let response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`Ошибка статус: ${response.status}`)
+        }
+        return await response.json()
+    }
+
+    getAllCharacters =  async () => {
+        let response = await this.getInfo(`https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=110&apikey=${this._apiKey}`)
+        return response.data.results.map(this._transformData)
+    }
+
+    getCharacter = async (id) => {
+        let response = await this.getInfo(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${this._apiKey}`)
+        return this._transformData(response.data.results[0])
+    }
+
+    _transformData = (char) => {
+        return {
+            name: char.name,
+            description: char.description && char.description.length > 50 ? char.description.slice(0,51) + "..." : "Описания нет",
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelInfo
